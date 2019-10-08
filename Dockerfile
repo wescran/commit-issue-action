efd431f24cc3d17fbeea9d0e6e5f1490138e6651
wescran <w.cranston@psiphon.ca>
2019-10-07 23:09:11 -0400

    Added files for action

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM alpine:3.10
+
+COPY get_commits.sh /get_commits.sh
+COPY issue_template.json /issue.template.json
+
+RUN apk --update add jq && \
+    apk --update add curl
+
+ENTRYPOINT ["/get_commits.sh"]
---
+++ get_commits.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+API="https://api.github.com"
+
+create_issue()
+{
+  urls=""
+  for u in $commiturls; do
+    urls=$urls'<br />'"$u"
+  done
+  json=$(jq -rc --arg src "$INPUT_SOURCEREPO" --arg urls "$urls" '.title += $src | .body += $urls' issue_template.json)
+  issue=$(curl -sS -X POST "$API/repos/$INPUT_TARGETREPO/issues" -H "authorization: token $GITHUB_TOKEN" -H "Content-Type: application/json" -d "$json")
+  created=$?
+  if [ $created -ne 0 ]; then
+    echo "Error with request: $issue"
+    return $check
+  else
+    echo "Issue created in $INPUT_TARGETREPO"
+    issueURL=$(echo $issue | tr '\n' ' ' | jq -r '.html_url')
+    return 0
+  fi
+}
+
+check_commits()
+{
+  commits=$(curl -sS "$API/repos/$INPUT_SOURCEREPO/commits?since=$(date -Is -d '1 hour ago')&until=$(date -Is)")
+  check=$?
+  if [ $check -ne 0 ]; then
+    echo "Error with request: $commits"
+    return $check
+  fi
+  if [ "$(echo $commits)" = "[ ]" ]; then
+    return 1
+  else
+    commiturls=$(echo $commits | tr '\n' ' ' | jq -r ".[] | .html_url")
+    return 0
+  fi
+}
+
+main()
+{
+  echo "Checking for recent commits to $INPUT_SOURCEREPO"
+  check_commits
+  found=$?
+  if [ $found -eq  0 ]; then
+    echo "There is a new commit to $INPUT_SOURCEREPO! Creating issue in $INPUT_TARGETREPO."
+    create_issue
+    result=$?
+    if [ $result -eq 0 ]; then
+      echo ::set-output name=issue_url::$issueURL
+    else
+      echo ::set-output name=issue_url::$result 
+    fi
+  else
+    echo "No recent commits found."
+    echo ::set-output name=issue_url::$found 
+  fi
+}
+
+#begin here
+main
